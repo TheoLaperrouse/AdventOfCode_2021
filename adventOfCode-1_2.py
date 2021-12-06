fdcd85a979560728ebd145bd814157d5768787e9
Théo Laperrouse <theo.laperrouse@energiency.com>
2021-12-06 13:09:02 +0100

    Day One

--- adventOfCode-1_2.py
+++ adventOfCode-1_2.py
@@ -3,10 +3,11 @@ file = open('input.txt', "r")
 lines = file.readlines()
 file.close()
 compteur = 0
-line_prec = 0
+sum_lines_prec = -1
 for index, line in enumerate(lines):
-    intLine = int(line)
-    if line_prec < intLine:
-        compteur += 1
-    line_prec = intLine
+    if index + 2 < len(lines):
+        sum_lines = int(line) + int(lines[index+1]) + int(lines[index+2])
+        if sum_lines_prec < sum_lines:
+            compteur += 1
+        sum_lines_prec = sum_lines
 print(compteur - 1)
