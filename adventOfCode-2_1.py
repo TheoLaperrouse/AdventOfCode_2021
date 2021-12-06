1a27645c8140304d0153af94cac559a4383990d8
Théo Laperrouse <theo.laperrouse@energiency.com>
2021-12-06 13:26:26 +0100

    Day Two

---
+++ adventOfCode-2_1.py
@@ -0,0 +1,15 @@
+file = open('input.txt', "r")
+lines = file.readlines()
+file.close()
+x = 0
+depth = 0
+for line in lines:
+    instruction = line.split()
+    if instruction[0] == "forward":
+        x += int(instruction[1])
+    if instruction[0] == "down":
+        depth += int(instruction[1])
+    if instruction[0] == "up":
+        depth -= int(instruction[1])
+print(f'{x}, {depth}')
+print(f'Answer : {x*depth}')
---
+++ adventOfCode-2_2.py
@@ -0,0 +1,18 @@
+file = open('input.txt', "r")
+lines = file.readlines()
+file.close()
+x = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    instruction = line.split()
+    if instruction[0] == "forward":
+        x += int(instruction[1])
+        depth = depth + aim * int(instruction[1])
+    if instruction[0] == "down":
+        aim += int(instruction[1])
+    if instruction[0] == "up":
+        aim -= int(instruction[1])
+print(f'{x}, {depth}')
+print(f'Answer : {x*depth}')
