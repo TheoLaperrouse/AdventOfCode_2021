9457dd98ef1a8687901492dacc746862743b799b
Théo Laperrouse <theo.laperrouse@energiency.com>
2021-12-07 09:30:55 +0100

    Day Seven

--- adventOfCode-7_2.py
+++ adventOfCode-7_2.py
@@ -5,8 +5,8 @@ file = open('input.txt', "r")
 lines = file.readlines()
 file.close()
 
-minFuel = 0
-
+allFuels = []
+minFuel = 10**30
 horizontal_positions = [int(line) for line in lines[0].split(',')]
 maxPos = max(horizontal_positions)
 
@@ -14,9 +14,10 @@ for dest in range(maxPos):
     allFuel = 0
     for horizontal_pos in horizontal_positions:
         if dest > horizontal_pos:
-            allFuel += dest - horizontal_pos
+            allFuel += (dest - horizontal_pos) * (dest - horizontal_pos+1) / 2
         elif dest < horizontal_pos:
-            allFuel += horizontal_pos - dest
-    minFuel = allFuel
+            allFuel += (horizontal_pos - dest) * (horizontal_pos - dest+1) / 2
+    if allFuel < minFuel:
+        minFuel = allFuel
 
 print(minFuel)
